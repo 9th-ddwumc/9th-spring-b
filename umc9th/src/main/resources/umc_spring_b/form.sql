40f2e0bb8b3bcae86c0804f4f64a2ef2c4e57850
heejung <mishel7@naver.com>
2025-10-13 15:12:42 +0900

    Feat/Chapter4

---
+++ umc9th/src/main/resources/umc_spring_b/form.sql
@@ -0,0 +1,7 @@
+create table form
+(
+    form_id   bigint      not null
+        primary key,
+    form_name varchar(10) null
+);
+
---
+++ umc9th/src/main/resources/umc_spring_b/term.sql
@@ -0,0 +1,8 @@
+create table term
+(
+    id      bigint auto_increment
+        primary key,
+    content varchar(255) not null,
+    name    varchar(30)  not null
+);
+
