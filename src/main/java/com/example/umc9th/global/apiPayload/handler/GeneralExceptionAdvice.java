537e4b4380b8f1211ce290d8c7e08f17cb8c0fef
zhwan2845 <johaeun0802@naver.com>
2025-11-10 08:15:31 +0900

    feat(global): 500 에러 발생 시 Discord Webhook 자동 알림 기능 추가

--- src/main/java/com/example/umc9th/global/apiPayload/handler/GeneralExceptionAdvice.java
+++ src/main/java/com/example/umc9th/global/apiPayload/handler/GeneralExceptionAdvice.java
@@ -4,39 +4,48 @@ import com.example.umc9th.global.apiPayload.ApiResponse;
 import com.example.umc9th.global.apiPayload.code.BaseErrorCode;
 import com.example.umc9th.global.apiPayload.code.GeneralErrorCode;
 import com.example.umc9th.global.apiPayload.exception.GeneralException;
+import com.example.umc9th.global.util.DiscordNotifier;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GeneralExceptionAdvice {
+
+    private final DiscordNotifier discordNotifier;
 
     // 애플리케이션에서 발생하는 커스텀 예외를 처리
     @ExceptionHandler(GeneralException.class)
-    public ResponseEntity<ApiResponse<Void>> handleException(
-            GeneralException ex
+    public ResponseEntity<ApiResponse<Void>> handleGeneralException(
+            GeneralException ex,
+            HttpServletRequest request
     ) {
+        BaseErrorCode code = ex.getCode();
 
-        return ResponseEntity.status(ex.getCode().getStatus())
-                .body(ApiResponse.onFailure(
-                                ex.getCode(),
-                                null
-                        )
-                );
+        // 500 에러 발생 시 디스코드로 알림 전송
+        if (code == GeneralErrorCode.INTERNAL_SERVER_ERROR) {
+            discordNotifier.sendErrorMessage(ex, request.getRequestURI());
+        }
+
+        return ResponseEntity.status(code.getStatus())
+                .body(ApiResponse.onFailure(code, null));
     }
 
     // 그 외의 정의되지 않은 모든 예외 처리
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<String>> handleException(
-            Exception ex
+            Exception ex,
+            HttpServletRequest request
     ) {
-
         BaseErrorCode code = GeneralErrorCode.INTERNAL_SERVER_ERROR;
+
+        // 500 에러 발생 시 디스코드로 알림 전송
+        discordNotifier.sendErrorMessage(ex, request.getRequestURI());
+
         return ResponseEntity.status(code.getStatus())
-                .body(ApiResponse.onFailure(
-                                code,
-                                ex.getMessage()
-                        )
-                );
+                .body(ApiResponse.onFailure(code, ex.getMessage()));
     }
 }
